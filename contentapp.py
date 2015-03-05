e360073afc5d59d17f96dcbfdd4cf47989b0110d
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-03-05 12:20:26 +0100

    Con Post añadimos o actualizamos el contenido

--- contentapp.py
+++ contentapp.py
@@ -27,23 +27,37 @@ class contentApp (webapp.webApp):
     def parse(self, request):
         """Return the resource name (including /)"""
 
-        return request.split(' ', 2)[1]
-
-    def process(self, resourceName):
+        metodo = request.split(' ', 2)[0]
+        recurso = request.split(' ',2)[1]
+        #EL CUERPO DEL POST DEL FORMULARIO VENDRA DE LA FORMA DE campo=valor1&campo2=valor2....
+        if metodo == "POST":
+            cuerpo = request.split('\r\n\r\n', 1)[1]
+        else:
+            cuerpo = ""
+        return (metodo, recurso, cuerpo)
+    def process(self, (metodo, recurso, cuerpo)):
         """Process the relevant elements of the request.
 
         Finds the HTML text corresponding to the resource name,
         ignoring requests for resources not in the dictionary.
         """
+        #CONSTRUCCION DEL FORMULARIO QUE VA A IR SIEMPRE DESPUES DE LA PAGINA
+        formulario = "<form method='post'>Dale un contenido:<input type='text' name='contenido' value='' /><button type='submit'>Enviar Contenido</button></form>"
+        
+        if metodo == "GET":
+            if recurso in self.content:
+                httpCode = "200 OK"
+                htmlBody = "<html><body>" + self.content[recurso] + formulario + "</body></html>"
 
-        if resourceName in self.content.keys():
-            httpCode = "200 OK"
-            htmlBody = "<html><body>" + self.content[resourceName] \
-                + "</body></html>"
-        else:
-            httpCode = "404 Not Found"
-            htmlBody = "Not Found"
-        return (httpCode, htmlBody)
+            else:
+                httpCode = "404 Not Found"
+                htmlBody = "<html><body>Not Found" + formulario + "</body></html>"
+            return (httpCode, htmlBody)
+        #CON POST PONGO O ACTUALIZO CONTENIDO A LA PAGINA
+        elif metodo == "POST":
+            contenido = cuerpo.split("=")[1]
+            self.content[recurso] = contenido
+            return ("200 OK", "<html><body>" + self.content[recurso] + formulario + "</body></html>")
 
 
 if __name__ == "__main__":
